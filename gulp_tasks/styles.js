331fd134a486e495155671717372e78d5739327b
Alexandre Jarnoux <alex.jarnoux@gmail.com>
2017-06-27 14:41:13 +1000

    Default variables: Force LESS files compilation order

--- gulp_tasks/styles.js
+++ gulp_tasks/styles.js
@@ -22,6 +22,9 @@ function styles() {
   };
 
   var injectFiles = gulp.src([
+    conf.path.src('/app/stylesheets/theme.less'),
+    conf.path.src('/app/stylesheets/*.less'),
+    conf.path.src('/app/stylesheets/variables.less'),
     conf.path.src('/app/**/*.less'),
     conf.path.src('/fonts/**/*.less'),
     conf.path.src('/images/**/*.less'),
