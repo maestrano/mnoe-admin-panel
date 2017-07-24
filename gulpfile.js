87da794cd5fd445702d1533357e2590df214675e
Alexandre Jarnoux <alex.jarnoux@gmail.com>
2017-07-24 12:02:53 +1000

    Build: display version in console and JS file header

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ const hub = new HubRegistry([conf.path.tasks('*.js')]);
 // Tell gulp to use the tasks just loaded
 gulp.registry(hub);
 
-gulp.task('inject', gulp.series(gulp.parallel('styles', 'scripts'), 'inject'));
+gulp.task('inject', gulp.series(gulp.parallel('styles', 'scripts'), 'version', 'inject'));
 gulp.task('build', gulp.series('partials', gulp.parallel('inject', 'fonts', 'images', 'other'), 'build'));
 gulp.task('test', gulp.series('scripts', 'karma:single-run'));
 gulp.task('test:auto', gulp.series('watch', 'karma:auto-run'));
